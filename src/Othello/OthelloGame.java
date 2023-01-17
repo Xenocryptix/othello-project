ee82db748548992636eb899b66432f6bf64ee6bc
Moamen <m.elkayal@student.utwente.nl>
2023-01-17 16:09:13 +0100

    OthelloGame

--- src/Othello/OthelloGame.java
+++ src/Othello/OthelloGame.java
@@ -1,4 +1,66 @@
 package Othello;
 
-public class OthelloGame {
+import java.util.List;
+
+public class OthelloGame implements Game{
+    /**
+     * Check if the game is over, i.e., there is a winner or no more moves are available.
+     *
+     * @return whether the game is over
+     */
+    @Override
+    public boolean isGameover() {
+        return true;
+    }
+
+    /**
+     * Query whose turn it is
+     *
+     * @return the player whose turn it is
+     */
+    @Override
+    public Player getTurn() {
+        return null;
+    }
+
+    /**
+     * Get the winner of the game. If the game is a draw, then this method returns null.
+     *
+     * @return the winner, or null if no player is the winner
+     */
+    @Override
+    public Player getWinner() {
+        return null;
+    }
+
+    /**
+     * Check if a move is a valid move
+     *
+     * @param move the move to check
+     * @return true if the move is a valid move
+     */
+    @Override
+    public boolean isValidMove(Move move) {
+        return false;
+    }
+
+    /**
+     * Return all moves that are valid in the current state of the game
+     *
+     * @return the list of currently valid moves
+     */
+    @Override
+    public List<Move> getValidMoves() {
+        return null;
+    }
+
+    /**
+     * Perform the move, assuming it is a valid move.
+     *
+     * @param move the move to play
+     */
+    @Override
+    public void doMove(Move move) {
+
+    }
 }
