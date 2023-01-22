74d7ad1c360cdcec0e9f363972503b89be26b199
xenocryptix <minhhieuchu1012@gmail.com>
2023-01-22 22:37:29 +0100

    oopsie

--- src/Othello/ai/Factory.java
+++ src/Othello/ai/Factory.java
@@ -1,4 +1,18 @@
 package Othello.ai;
 
+import Othello.Player;
+
 public interface Factory {
+    /**
+     * Make a new human player
+     * @return human player object
+     */
+    Player makeHumanPlayer();
+
+    /**
+     * Make a new computer player with specified strategy
+     * @param strategy
+     * @return computer player object
+     */
+    Player makeComputerPlayer(Strategy strategy);
 }
--- src/Othello/ai/Strategy.java
+++ src/Othello/ai/Strategy.java
@@ -3,16 +3,5 @@ package Othello.ai;
 import Othello.Player;
 
 public interface Strategy {
-    /**
-     * Make a new human player
-     * @return human player object
-     */
-    Player makeHumanPlayer();
 
-    /**
-     * Make a new computer player with specified strategy
-     * @param strategy
-     * @return computer player object
-     */
-    Player makeComputerPlayer(Strategy strategy);
 }
