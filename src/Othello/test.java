bcafb12a385de0a1b6d1a1c7d5706e29416d18b1
Moamen <m.elkayal@student.utwente.nl>
2023-01-25 11:35:46 +0100

    test game

--- src/Othello/test.java
+++ src/Othello/test.java
@@ -1,5 +1,6 @@
 package Othello;
 
+import Othello.model.Disk;
 import Othello.model.Game;
 import Othello.model.OthelloGame;
 import Othello.players.ai.ComputerPlayer;
@@ -11,17 +12,18 @@ import Othello.players.Player;
 public class test {
     public static void main(String[] args) {
         Game game = new OthelloGame();
-        Player p1 = new PlayerFactory().makeComputerPlayer(new GreedyStrategy());
-        Player p2 = new PlayerFactory().makeComputerPlayer(new NaiveStrategy());
+        Player p1 = new PlayerFactory().makeComputerPlayer(new NaiveStrategy());
+        Player p2 = new PlayerFactory().makeComputerPlayer(new GreedyStrategy());
         ((OthelloGame) game).setPlayer1(p1);
         ((OthelloGame) game).setPlayer1(p2);
-        System.out.println(game);
         while (!game.isGameover()) {
             game.doMove(((ComputerPlayer) p1).determineMove(game));
-            System.out.println(game);
+            System.out.println(game.toString());
             game.doMove(((ComputerPlayer) p2).determineMove(game));
-            System.out.println(game);
+            System.out.println(game.toString());
         }
-
+        System.out.println(((OthelloGame) game).getBoard().countDisk(Disk.BLACK));
+        System.out.println(((OthelloGame) game).getBoard().countDisk(Disk.WHITE));
+        System.out.println(game.getWinner());
     }
 }
