395312b785dc94ae38aa288942a7da2f386eb834
xenocryptix <minhhieuchu1012@gmail.com>
2023-01-30 21:51:40 +0100

    Changes by momen

---
+++ src/Othello/Server/Matchmaking.java
@@ -0,0 +1,24 @@
+package Othello.Server;
+
+import Othello.Server.ClientHandler;
+import Othello.Server.OthelloServer;
+import Othello.Server.Server;
+
+import java.util.List;
+
+public class Matchmaking implements Runnable {
+    private List<ClientHandler> queue;
+    private OthelloServer server;
+    public Matchmaking(OthelloServer server) {
+        this.server = server;
+    }
+    @Override
+    public void run() {
+        while (server.isAccepting()) {
+            queue = server.getQueue();
+            if (server.getInQueue() > 1) {
+                server.startGame();
+            }
+        }
+    }
+}
