09af6c4d68f608428d0429fc0f06f6023ed63094
Moamen <m.elkayal@student.utwente.nl>
2023-02-02 15:13:59 +0100

    tests

--- test/NetworkingTest/ExceptionTest.java
+++ test/NetworkingTest/ExceptionTest.java
@@ -1,17 +1,8 @@
 package NetworkingTest;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import othello.controller.client.OthelloClient;
-import othello.controller.server.ClientHandler;
 import othello.controller.server.OthelloServer;
-import othello.exceptions.ConnectionDropped;
 import othello.exceptions.PortNumberException;
-
-import java.io.*;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -31,30 +22,4 @@ public class ExceptionTest {
         assertThrows(PortNumberException.class, () -> testServer.start(), "The specified port is invalid or unavailable");
     }
 
-    @Test
-    public void testConnectionDroppedExcetion() throws ConnectionDropped, IOException, PortNumberException {
-        OthelloServer server = new OthelloServer(0);
-        server.start();
-        Socket client1 = new Socket(InetAddress.getLocalHost(),server.getPort());
-        ClientHandler clientHandler1 = new ClientHandler(client1,server);
-        Socket client2 = new Socket(InetAddress.getLocalHost(),server.getPort());
-        ClientHandler clientHandler2 = new ClientHandler(client2,server);
-
-        try (BufferedReader reader2 = new BufferedReader(new InputStreamReader(client2.getInputStream()));
-             PrintWriter writer2 = new PrintWriter(new OutputStreamWriter(client2.getOutputStream()), true);
-             BufferedReader reader1 = new BufferedReader(new InputStreamReader(client1.getInputStream()));
-             PrintWriter writer1 = new PrintWriter(new OutputStreamWriter(client1.getOutputStream()), true)) {
-            writer1.println("HELLO");
-            writer2.println("HELLO");
-            writer1.println("LOGIN~MO");
-            writer2.println("LOGIN~WO");
-
-            writer1.println("QUEUE");
-            writer2.println("QUEUE");
-
-
-            assertThrows(ConnectionDropped.class, () -> server.removeClient(clientHandler1));
-        }
-
-    }
 }
